5e244a73bbe34466e583506bda35c85c59dcb701
Haniel Rameshbabu <haniel.dev@gmail.com>
2018-10-12 17:26:35 +0100

    moved to CGS D3-D1 directory

---
+++ CGS_D3_D1/README.md
@@ -0,0 +1,10 @@
+# OS Assessment
+
+Attempting to use github to store Assessment files.
+
+## CGS D3-D1
+
+Solved!
+
+
+## CGS C3-C1
---
+++ CGS_D3_D1/shell.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "filesys.h"
+
+void main(int argc, char const *argv[]) {
+  format();
+  writedisk("virtualdiskD3_D1");
+
+}
